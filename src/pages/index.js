901ef974a1aaa675cb2fb7d9fbbc62866cdc272d
sabbir1054 <mdsabbir1054@gmail.com>
2023-07-22 15:58:07 +0600

    fetch data by getfetch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
-import Head from "next/head";
 import RootLayout from "@/components/Layouts/RootLayout";
+import AllNews from "@/components/UI/AllNews";
 import Banner from "@/components/UI/Banner";
+import Head from "next/head";
 
-
-const HomePage = () => {
+const HomePage = ({ allNews }) => {
+  console.log(allNews);
   return (
     <>
       <Head>
@@ -15,7 +16,9 @@ const HomePage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Banner />
+      <AllNews allNews={allNews} />
     </>
   );
 };
@@ -23,4 +26,16 @@ export default HomePage;
 
 HomePage.getLayout = function getLayout(page) {
   return <RootLayout>{page}</RootLayout>;
+};
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/news/");
+  const data = await res.json();
+  // console.log(data);
+
+  return {
+    props: {
+      allNews: data,
+    },
+  };
 };
